3b0786a36a12324fb311cc3d999063d1b90fa3b3
Abdulaziz <abdulazizakbarov065@gmail.com>
2024-10-17 19:38:58 +0500

    Tugadi

---
+++ main.js
@@ -0,0 +1,92 @@
+//1-Misol//
+
+
+
+// let matn =prompt("Soz Kirting");
+
+// alert(matn.toUpperCase() +" " + matn.toLowerCase());
+
+
+
+//1-Tugashi//
+
+
+
+//2-Misol//
+
+
+// let jumla =prompt("Jumla Kirgizing");
+
+// let soz = jumla;
+
+// if (jumla == "") {
+//      alert("Jumla kiritmading!!!");
+//     }
+//     else{
+
+
+//         let soz =prompt("Topmoqchi bolgan sozingizni kirgizing");
+
+
+//         if(soz&&jumla.includes(soz)){
+        
+//             alert(`Siz Kiritgan soz ${soz}`);
+        
+//         }
+//         else{
+//             alert("Siz kirtgan soz topilmadi");
+//         }
+        
+//     }
+
+
+
+//2-Tugashi//
+
+
+
+//3-Misol//
+
+// let matn =prompt(" Prabellar Bilan Matn Kirgizing");
+
+
+// alert(matn.length);
+
+// let yoq = matn.trim();
+
+// alert("Matn Probellarsiz holati " + yoq);
+
+// alert( "Probellersiz uzunligi "+ yoq.length);
+
+
+
+//3-Tugashi//
+
+
+//4-Misol//
+
+
+// let matn = prompt("Matn kiriting");
+// let qidir = matn.search(" ");
+// alert("Probelning joylashishi " + qidir);
+// let natija = matn.trim() + " " + matn.trim();
+// alert(
+//   "Natija ->" + natija
+// );
+
+//4-Tugashi//
+
+
+//5-Misol//
+
+// let soz = prompt("Sevimli so'zingizni kiriting :");
+// let a = soz + " ";
+// let probel = a.repeat(5).trim();
+// alert("5 martta qaytarilgan holati " + probel);
+
+// let boshqasoz = prompt("Boshqa so'zni kiriting :");
+// alert("Sevimli so'zingiz  " + a + "  " + "Boshqa kiritgan so'zingiz  " + boshqasoz);
+
+
+
+//5-Tugashi//
